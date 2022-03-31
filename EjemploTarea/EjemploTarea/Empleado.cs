dbd32ccde120c92785b8b8d42a79ecf9311730d4
Wilmer-hdz <wilmjoshhdz00@gmail.com>
2022-03-31 14:29:39 -0600

    Archivo tarea 1

---
+++ EjemploTarea/EjemploTarea/Empleado.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploTarea
+{
+    public abstract class Empleado
+    {
+        private string primerNombre;
+        private string apellidoPaterno;
+        private string numeroSeguroSocial;
+
+        public Empleado(string nombre, string apellido, string nss)
+        {
+            primerNombre = nombre;
+            apellidoPaterno = apellido;
+            numeroSeguroSocial = nss;
+        }
+
+        public string PrimerNombre
+        {
+            get
+            {
+                return primerNombre;
+            }
+        }
+
+        public string ApellidoPaterno
+        {
+            get
+            {
+                return apellidoPaterno;
+            }
+        }
+
+        public string NumeroSeguroSocial
+        {
+            get
+            {
+                return numeroSeguroSocial;
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1} \nnumero de seguro social: {2}", 
+                primerNombre, apellidoPaterno, numeroSeguroSocial);
+        }
+
+        public abstract decimal Ingresos(); 
+    }
+}
---
+++ EjemploTarea/EjemploTarea/EmpleadoBaseMasComision.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploTarea
+{
+    public class EmpleadoBaseMasComision : EmpleadoPorComision
+    {
+        private decimal salarioBase;
+        public EmpleadoBaseMasComision(string nombre, string apellido, string nss,
+            decimal ventas, decimal tarifa, decimal salario) : 
+            base(nombre, apellido, nss, tarifa, salario)
+        {
+            salarioBase = salario;
+        }
+
+        public decimal SalarioBase
+        {
+            get { return salarioBase; }
+            set { salarioBase = (value >= 0) ? value : 0; }
+        }
+
+        public override decimal Ingresos()
+        {
+            return SalarioBase + base.Ingresos();
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1}; {2}: {3:C}", "salario base +", 
+                base.ToString(), "salario base", SalarioBase);
+        }
+    }
+}
